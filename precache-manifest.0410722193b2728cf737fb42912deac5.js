06d188a22bb95d7fff487159cfbc6ddba280a251
Bruno Lourenco <brunnolou@gmail.com>
2018-10-31 11:05:22 +0100

    Updates

---
+++ precache-manifest.0410722193b2728cf737fb42912deac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b400105d0d47d6a025c9",
+    "url": "/static/js/main.b400105d.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "b400105d0d47d6a025c9",
+    "url": "/static/css/main.188a9b55.chunk.css"
+  },
+  {
+    "revision": "428d0b94ec192eac9b0d16f74236577a",
+    "url": "/index.html"
+  }
+];
